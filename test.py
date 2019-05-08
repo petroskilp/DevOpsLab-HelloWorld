aa714a583f502dde26301fc5842eba08fc162cca
petroskilp <petroski@ead.uepg.br>
2019-05-08 18:03:27 -0300

    Update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Engenharia de Software")
+        self.assertRegex(result.data.decode(), "Iniciando com DevOps Python")
 
 
 if __name__ == "__main__":
